4d72f61acd9c2b8621e4043d2de1474b69306e2f
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-13 12:32:40 +0200

    use resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,19 @@ Rails.application.routes.draw do
   # delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Create (get the form)
-  get "tasks/new", to: "tasks#new", as: "new_task"
+  # get "tasks/new", to: "tasks#new", as: "new_task"
   # Delete
-  delete "tasks/:id", to: "tasks#destroy"
+  # delete "tasks/:id", to: "tasks#destroy"
   # Read (all)
-  get "tasks", to: "tasks#index", as: "tasks"
+  # get "tasks", to: "tasks#index", as: "tasks"
   # Read (one)
-  get "tasks/:id", to: "tasks#show", as: "task"
+  # get "tasks/:id", to: "tasks#show", as: "task"
   # Create (post the form)
-  post "tasks", to: "tasks#create"
+  # post "tasks", to: "tasks#create"
   # Update (get the form content)
-  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   # Update (patch the form)
-  patch "tasks/:id", to: "tasks#update"
+  # patch "tasks/:id", to: "tasks#update"
+
+  resources :tasks, except: [:destroy]
 end
